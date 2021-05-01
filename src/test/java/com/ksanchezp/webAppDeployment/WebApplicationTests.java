ab37a54dede3385a3bfefe0133cf2cc9cc0d8f99
BlackKat <katsanch34@gmail.com>
2021-05-01 21:28:10 +0200

    create basic web App (home page)

---
+++ src/test/java/com/ksanchezp/webAppDeployment/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksanchezp.webAppDeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
